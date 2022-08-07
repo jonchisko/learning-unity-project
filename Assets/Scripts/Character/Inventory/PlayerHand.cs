b59d9d70f57d1b40892e31321d9a0f404b024ff9
jon škoberne <jonchisko@gmail.com>
2022-08-07 21:34:25 +0200

    Implement playerhand

--- Assets/Scripts/Character/Inventory/PlayerHand.cs
+++ Assets/Scripts/Character/Inventory/PlayerHand.cs
@@ -1,22 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Waffle.CardSystems.Item.Usable;
+using Waffle.Common;
 
 namespace Waffle.CharacterSystems.InventorySystems
 {
     public class PlayerHand : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        private GameObject handObject;
+        private GameObject equipableReference = null;
+        private IInventorySystem inventorySystem;
+        private Usable currentUsable = null;
+        
 
+        public PlayerHand(IInventorySystem inventorySystem, GameObject handObject)
+        {
+            this.inventorySystem = inventorySystem;
+            this.handObject = handObject;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void SetEquipableToHand(GameObject equipable)
         {
+            if (equipable == equipableReference) return;
 
+            equipableReference = equipable;
+            Instantiate(equipable, handObject.transform);
+            equipable.transform.SetParent(handObject.transform, false);
+        }
+
+        public void UnsetEquipableToHand()
+        {
+            Destroy(equipableReference);
+            equipableReference = null;
+        }
+
+        public void UsePotion()
+        {
+            Optional<Usable> usable = inventorySystem.UsePotion();
+            if (!usable.HasValue) return;
+
+            usable.Value.Use();
+        } 
+
+        public void EquipWeapon()
+        {
+            Optional<Usable> usable = inventorySystem.UseWeapon();
+            if (!usable.HasValue) return;
+
+            currentUsable = usable.Value;
+        }
+
+        public void UnequipWeapon()
+        {
+            if (currentUsable == null) return;
+
+            currentUsable = null;
+        }
+
+        public void PutToInventory()
+        {
+            if (currentUsable == null) return;
+
+            inventorySystem.AddWeapon(currentUsable);
+            currentUsable = null;
+        }
+
+        public void UseEquiped()
+        {
+            if (currentUsable == null) return;
+
+            currentUsable.Use();
         }
     }
 }
