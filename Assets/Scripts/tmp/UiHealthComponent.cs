df994ab51a5a48e73a80b5c94d375cfc61eee14a
jon škoberne <jonchisko@gmail.com>
2022-08-16 22:33:49 +0200

    Improve damage notification ui

--- Assets/Scripts/tmp/UiHealthComponent.cs
+++ Assets/Scripts/tmp/UiHealthComponent.cs
@@ -6,20 +6,60 @@ using TMPro;
 
 public class UiHealthComponent : MonoBehaviour
 {
+    class PoolTextObject
+    {
+        public GameObject damageTextObject;
+        public TextMeshProUGUI textMesh;
+    }
+
+    [SerializeField]
+    private int poolSize = 5;
     [SerializeField]
     private GameObject damageTextObject;
+    [SerializeField]
+    private GameObject spawnPosition;
 
-    private TextMeshProUGUI textMesh;
+    private List<PoolTextObject> poolTextObjects;
 
     private void Awake()
     {
-        textMesh = damageTextObject.GetComponentInChildren<TextMeshProUGUI>();
-        Debug.Assert(textMesh != null);
+        poolTextObjects = new List<PoolTextObject>(poolSize);
+        CreatePool();
     }
 
     public void OnObjectDamaged(int amount)
     {
-        textMesh.text = $"-{amount}";
-        damageTextObject.SetActive(true);
+        PoolTextObject poolTextObject = GetFreeInPool();
+        if (poolTextObject == null) return;
+
+        poolTextObject.textMesh.text = $"-{amount}";
+        poolTextObject.damageTextObject.SetActive(true);
+    }
+
+    private void CreatePool()
+    {
+        for(int i = 0; i < poolSize; i++)
+        {
+            GameObject gameObject = Instantiate(damageTextObject, spawnPosition.transform);
+            gameObject.SetActive(false);
+            gameObject.transform.SetParent(spawnPosition.transform, false);
+
+            PoolTextObject poolTextObject = new PoolTextObject
+            {
+                damageTextObject = gameObject,
+                textMesh = gameObject.GetComponentInChildren<TextMeshProUGUI>()
+            };
+
+            poolTextObjects.Add(poolTextObject);
+        }
+    }
+
+    private PoolTextObject GetFreeInPool()
+    {
+        foreach(var poolTextObject in poolTextObjects)
+        {
+            if (!poolTextObject.damageTextObject.activeSelf) return poolTextObject;
+        }
+        return null;
     }
 }
